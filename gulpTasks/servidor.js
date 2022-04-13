8fe0ec7efc41b0f7e95f239edeb6df2b379122e2
gdantasc <gabrieldcavalcante00@gmail.com>
2022-04-13 16:37:27 -0300

    feat: servidor tasks

--- gulpTasks/servidor.js
+++ gulpTasks/servidor.js
@@ -1,11 +1,19 @@
 const gulp = require("gulp");
+const webserver = require("gulp-webserver");
+const watch = require("gulp-watch");
 
-function monitorFiles(cb) {
-  return cb();
+function servidor() {
+  return gulp.src("build").pipe(
+    webserver({
+      port: 8080,
+      open: true,
+      livereload: true,
+    })
+  );
 }
 
-function servidor(cb) {
-  return cb();
+function monitorFiles() {
+  return watch("src/**/*.html", () => gulp.series("appHTML")());
 }
 
 module.exports = {
