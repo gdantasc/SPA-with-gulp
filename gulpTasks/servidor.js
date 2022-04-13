b2f7daf5aafa85a475c24847df9f54cbf66e6a46
gdantasc <gabrieldcavalcante00@gmail.com>
2022-04-13 15:52:14 -0300

    feat: build structure

---
+++ gulpTasks/servidor.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+
+function monitorFiles(cb) {
+  return cb();
+}
+
+function servidor(cb) {
+  return cb();
+}
+
+module.exports = {
+  monitorFiles,
+  servidor,
+};
